798c08e1bee9ef1afbda188aafbb1d0ba9a78088
AndreConforti <andre.r.m.conforti@gmail.com>
2023-04-13 16:41:40 -0300

    Fim da segunda aula

--- eventos/views.py
+++ eventos/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.urls import reverse
+from django.conf import settings
 
 from .models import Evento
+import csv, os
+from secrets import token_urlsafe
 
 
 # A view novo_evento, só pode ser acessada por usuário logado. 
@@ -82,3 +85,42 @@ def inscrever_evento(request, id):
         return redirect(reverse('eventos:inscrever_evento', kwargs={'id':id}))
     
 
+def participantes_evento(request, id):
+    # buscando na tabela Evento pelo id passado como parâmetro
+    # se não encontrar, devolva uma "página não encontrada" 
+    evento = get_object_or_404(Evento,id=id)
+    # verifica se o evento pertence ao criador do evento
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+
+    if request.method == "GET":
+        # o evento tem o campo participantes, então é só acessá-lo
+        participantes = evento.participantes.all()[:3]
+        return render(request, 'participantes_evento.html', {'evento': evento,
+                                                             'participantes':participantes,})
+
+
+def gerar_csv(request, id):
+    # buscando na tabela Evento pelo id passado como parâmetro
+    # se não encontrar, devolva uma "página não encontrada" 
+    evento = get_object_or_404(Evento,id=id)
+    # verifica se o evento pertence ao criador do evento
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+    
+    # para gerar um csv com todos os participantes, é necessário buscar todos eles
+    participantes = evento.participantes.all()
+
+    # token vai gerar um nome aleatório de 6 caracteres, toda vez que for gerado um arquivo .csv
+    token = f"{token_urlsafe(6)}.csv"
+    # mostra o caminho onde vai ser armazenado esse arquivo, junto com o seu nome
+    path = os.path.join(settings.MEDIA_ROOT, token)
+    # cria o csv (para escrita)
+    with open(path, 'w') as arq:
+        writer = csv.writer(arq, delimiter=';')
+        for participante in participantes:
+            x = (participante.username, participante.email)
+            writer.writerow(x)
+    
+    return redirect(f"/media/{token}")
+    
