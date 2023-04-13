fa9c5b5ee262696b82f1c078a2d0468348ad3551
AndreConforti <andre.r.m.conforti@gmail.com>
2023-04-12 22:49:14 -0300

    Criada a participacao do usuário no evento

--- eventos/urls.py
+++ eventos/urls.py
@@ -6,4 +6,6 @@ app_name = 'eventos'
 
 urlpatterns = [
     path('novo_evento/', views.novo_evento, name='novo_evento'),
+    path('gerenciar_evento/', views.gerenciar_evento, name='gerenciar_evento'),
+    path('inscrever_evento/<int:id>/', views.inscrever_evento, name='inscrever_evento'),
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -43,3 +43,42 @@ def novo_evento(request):
         
         messages.add_message(request, constants.SUCCESS, 'Evento cadastrado com sucesso')
         return redirect(reverse('eventos:novo_evento'))
+    
+
+@login_required
+def gerenciar_evento(request):
+    if request.method == "GET":
+        # cria uma variável para pegar o título do evento na busca
+        nome = request.GET.get('nome') 
+        # cria uma variável para receber os dados dos eventos que foram cadastrados
+        # pelo usuário que está logado
+        eventos = Evento.objects.filter(criador=request.user)
+        
+        if nome:
+            # filtra entre todos os eventos do usário logado, onde no nome,
+            # CONTENHA os dados enviados na variável de busca
+            eventos = eventos.filter(nome__contains=nome)
+
+        return render(request, 'gerenciar_evento.html', {'eventos':eventos})
+    
+
+@login_required
+def inscrever_evento(request, id):
+    # buscando na tabela Evento pelo id passado como parâmetro
+    # se não encontrar, devolva uma "página não encontrada" 
+    evento = get_object_or_404(Evento,id=id)
+    if request.method == "GET":
+        return render(request, 'inscrever_evento.html', {'evento':evento})
+    elif request.method == "POST":
+        # O campo ManyToManyField permite adicionar (fazer uma relação entre outros campos)
+        # nesse caso co-realaciona o id do usuário logado ao id do evento selecionado
+
+        # TODO: validar se o usuário já é um participante
+        
+        evento.participantes.add(request.user)
+        evento.save()
+        messages.add_message(request, constants.SUCCESS, 'Inscriçao realizada com sucesso!')
+        # return redirect(f"/eventos/inscrever_evento/{evento.id}/") 
+        return redirect(reverse('eventos:inscrever_evento', kwargs={'id':id}))
+    
+
